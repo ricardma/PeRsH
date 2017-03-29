96a9622f49a1cb17bc6d40753b56aeda851eaa7a
ricardma <aluno18774@ipt.pt>
2017-03-29 11:37:40 +0100

    Criação dos Roles "Admin" e "Utilizador"

--- TrabFinal - PeRsH/TrabFinal - PeRsH/Controllers/AccountController.cs
+++ TrabFinal - PeRsH/TrabFinal - PeRsH/Controllers/AccountController.cs
@@ -159,8 +159,8 @@ namespace TrabFinal___PeRsH.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    // await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    await UserManager.AddToRoleAsync(user.Id, "Utilizador");
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
