634ea25dc70b0342ae78605cdb4d9eace24c3d8d
ricardma <aluno18774@ipt.pt>
2017-03-29 10:32:02 +0100

    Criação do Projeto

---
+++ TrabFinal - PeRsH/TrabFinal - PeRsH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrabFinal___PeRsH.MvcApplication" Language="C#" %>
---
+++ TrabFinal - PeRsH/TrabFinal - PeRsH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabFinal___PeRsH.Startup))]
+namespace TrabFinal___PeRsH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
